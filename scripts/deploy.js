66d6b3c3a20c328bd6bd76c22d7c6101f7420c16
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-07-27 21:05:38 +0545

    all completed...

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  // Contract deployed to address: 0x956eACA32F3DF8FEC5E1b4503455BEa89E45a557 
